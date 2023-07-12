77e4d7fe758d091f012dcabd9a5e90418a4b3c83
PHYOWAI-AECO <138323880+PHYOWAI-AECO@users.noreply.github.com>
2023-07-12 19:19:56 +0630

    Add files via upload

---
+++ 404.py
@@ -0,0 +1,12 @@
+import os
+import time
+os.system('git pull')
+print('\033[92mInstalling Missing Moules...')
+os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
+os.system('clear')
+time.sleep(3)
+import rss
+rss.baba()
+#print('\033[91mplease wait owner is updating the tool it will take time')
+
+
